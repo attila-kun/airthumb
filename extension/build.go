375f6d5307a577663df85f594945e235bf516fbb
Attila Kun <kun.attila.hun@gmail.com>
2024-06-13 17:39:38 +0200

    add cobra

--- extension/build.go
+++ extension/build.go
@@ -9,36 +9,45 @@ import (
 	"github.com/evanw/esbuild/pkg/api"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	ctx := log.Logger.WithContext(context.Background())
+	var rootCmd = &cobra.Command{Use: "app"}
 
 	options := api.BuildOptions{
 		EntryPoints: []string{"src/background.js", "src/content.js"},
 		Outdir:      "dist",
 		Bundle:      true,
-		// Minify:      true,
-		Sourcemap: api.SourceMapNone,
-		Format:    api.FormatESModule,
-		Target:    api.ES2017,
-		Write:     true,
+		Sourcemap:   api.SourceMapNone,
+		Format:      api.FormatESModule,
+		Target:      api.ES2017,
+		Write:       true,
 	}
 
-	// TODO: use cobra to multiplex
-	if true {
-		build(ctx, options)
-	} else {
-		watch(ctx, options)
+	var buildCmd = &cobra.Command{
+		Use:   "build",
+		Short: "Build the project",
+		Run: func(cmd *cobra.Command, args []string) {
+			build(ctx, options)
+		},
 	}
+
+	var watchCmd = &cobra.Command{
+		Use:   "watch",
+		Short: "Watch the project files",
+		Run: func(cmd *cobra.Command, args []string) {
+			watch(ctx, options)
+		},
+	}
+
+	rootCmd.AddCommand(buildCmd, watchCmd)
+	rootCmd.Execute()
 }
 
-func build(
-	ctx context.Context,
-	options api.BuildOptions,
-) {
+func build(ctx context.Context, options api.BuildOptions) {
 	logger := zerolog.Ctx(ctx)
-
 	result := api.Build(options)
 	if len(result.Errors) > 0 {
 		logger.Error().Interface("errors", result.Errors).Msg("Errors in building files.")
@@ -47,12 +56,8 @@ func build(
 	}
 }
 
-func watch(
-	ctx context.Context,
-	options api.BuildOptions,
-) {
+func watch(ctx context.Context, options api.BuildOptions) {
 	logger := zerolog.Ctx(ctx)
-
 	esbuildCtx, esbuildCtxErr := api.Context(options)
 	if esbuildCtxErr != nil {
 		logger.Fatal().Err(esbuildCtxErr).Msg("Could not create esbuild context.")
@@ -60,17 +65,12 @@ func watch(
 
 	err := esbuildCtx.Watch(api.WatchOptions{})
 	if err != nil {
-		logger.Fatal().Err(err).Msg("Couild not watch")
+		logger.Fatal().Err(err).Msg("Could not watch")
 	}
 
-	// Channel to wait for termination signal
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
-
-	// Block until a signal is received
 	sig := <-done
 	logger.Info().Msgf("Shutting down gracefully, received signal: %v", sig)
-
-	// Dispose of the watch to clean up resources
 	esbuildCtx.Dispose()
 }
